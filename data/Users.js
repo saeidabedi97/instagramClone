d718f31d4b1d3988a5f9a203d939bacc462e09d3
Saeid <48330269+saeidabedi97@users.noreply.github.com>
2022-12-14 22:08:21 +0100

    created the header and stories section

---
+++ data/Users.js
@@ -0,0 +1,22 @@
+export const USERS = [
+  {
+    user: "Alizaa",
+    image: "https://randomuser.me/api/portraits/men/88.jpg",
+  },
+  {
+    user: "Davidko",
+    image: "https://randomuser.me/api/portraits/men/37.jpg",
+  },
+  {
+    user: "ali57",
+    image: "https://randomuser.me/api/portraits/women/28.jpg",
+  },
+  {
+    user: "mahsashabani",
+    image: "https://randomuser.me/api/portraits/women/27.jpg",
+  },
+  {
+    user: "kashrafiq82",
+    image: "https://randomuser.me/api/portraits/women/36.jpg",
+  },
+];
